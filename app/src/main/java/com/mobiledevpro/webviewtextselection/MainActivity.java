138da89dad96963150a1967dc8585aebc57a0fbe
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2018-11-12 16:43:52 +0200

    Display html

--- app/src/main/java/com/mobiledevpro/webviewtextselection/MainActivity.java
+++ app/src/main/java/com/mobiledevpro/webviewtextselection/MainActivity.java
@@ -1,13 +1,32 @@
 package com.mobiledevpro.webviewtextselection;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.webView)
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initWebView();
+
+    }
+
+    private void initWebView() {
+        mWebView.loadUrl("http://www.mobile-dev.pro/webview_text_selection.html");
+
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
     }
 }
